22d3f9af94495f125fff9cf8f8ac8376b3c8283f
Andi Virgo <geubot@gmail.com>
2025-01-23 13:21:45 +0700

    Add files via upload

--- urln.js
+++ urln.js
@@ -1,47 +1,59 @@
-const buttonM = `<style>
-#show,#content{display:none;}
-#show:checked~#contentM{display:block;}
+const buttonM = `
+<style>
+#show, #contentM { display: none; }
+#show:checked ~ #contentM { display: block; }
 </style>
-<input id="show" type=checkbox>
-<label for="show" style="border:1px solid black; padding:8px;display:inline-block">MENU</label>
-<span  id="contentM"></span>
+<input id="show" type="checkbox">
+<label for="show" style="border:1px solid black; padding:8px; display:inline-block; cursor:pointer;">MENU</label>
+<div id="contentM"></div>
 `;
-document.body.appendChild(buttonM);
+
+document.body.insertAdjacentHTML('beforeend', buttonM);
+
 const content = document.getElementById('contentM');
 const divE = document.createElement('div');
 divE.setAttribute('id', 'data');
 divE.textContent = 'Loading......';
 content.appendChild(divE);
 
-
 divE.style.fontFamily = "sans-serif"; 
 divE.style.margin = "0"; 
 divE.style.padding = "0"; 
+
 const url = `https://api.github.com/repos/urln/urln.github.io/contents/post`;
-fetch(url).then(response =>{
-if(!response.ok){
-throw new Error('Network response was not ok ' + response.statusText);}
-return response.json();
-}).then(data =>{
-const dataDiv = document.getElementById('data');
-dataDiv.innerHTML = '';
-const ul = document.createElement('ul');
-ul.style.listStyleType = "none";ul.style.margin = "0";
-ul.style.padding = "0";
-data.forEach(item => {
-const oSt =`${item.html_url}`;
-const nSt = oSt.replace(/https:\/\/github.com\/urln\/urln.github.io\/blob\/main\/post\//g, "");
-const eNs = nSt.replace(/.html|.css|.js|.txt|.md|.png|/g,"");
-const li = document.createElement('li');
-li.style.backgroundColor = "#eee"; 
-li.style.margin = "1px 0"; 
-li.style.padding = "5px"; 
-li.style.border = "5px solid #ddd"; 
-li.style.borderRadius = "5px";
-li.innerHTML = `<a style="color:#123456" href="post/`+eNs+`.html">`+eNs+`</a> 
-<a style="float:right;color:#456789" href="${item.download_url}">raw</a>`;
-ul.appendChild(li);});dataDiv.appendChild(ul);
-}).catch(error => {
-const dataDiv = document.getElementById('data');
-dataDiv.innerHTML = 'Error fetching data: ' + error.message;
-});
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const dataDiv = document.getElementById('data');
+        dataDiv.innerHTML = '';
+        const ul = document.createElement('ul');
+        ul.style.listStyleType = "none";
+        ul.style.margin = "0";
+        ul.style.padding = "0";
+        
+        data.forEach(item => {
+            const oSt = `${item.html_url}`;
+            const nSt = oSt.replace(/https:\/\/github.com\/urln\/urln.github.io\/blob\/main\/post\//g, "");
+            const eNs = nSt.replace(/.html|.css|.js|.txt|.md|.png/g, "");
+            const li = document.createElement('li');
+            li.style.backgroundColor = "#eee"; 
+            li.style.margin = "1px 0"; 
+            li.style.padding = "5px"; 
+            li.style.border = "5px solid #ddd"; 
+            li.style.borderRadius = "5px";
+            li.innerHTML = `<a style="color:#123456" href="post/${eNs}.html">${eNs}</a> 
+                            <a style="float:right;color:#456789" href="${item.download_url}">raw</a>`;
+            ul.appendChild(li);
+        });
+        
+        dataDiv.appendChild(ul);
+    })
+    .catch(error => {
+        const dataDiv = document.getElementById('data');
+        dataDiv.innerHTML = 'Error fetching data: ' + error.message;
+    });
