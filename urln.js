130e235a49d84782e5243aa3abeb58392d7fa709
urln <geubot@gmail.com>
2025-02-09 13:34:46 +0700

    Add files via upload

--- urln.js
+++ urln.js
@@ -1,42 +1,58 @@
+const $new = document.createElement.bind(document);
+const $id = document.getElementById.bind(document);
 const buttonM = `
 <style>
-.btn{width:40px;height:40px;overflow:hidden;position:fixed;
-top:15px;right:3%;z-index:1;display:inline-block;transition:1s;
-background-image:url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><rect width="48" height="48" fill="none"/><line x1="24" y1="8" x2="24" y2="40" stroke="black" stroke-width="6"/><line x1="8" y1="24" x2="40" y2="24" stroke="black" stroke-width="6"/></svg>');
+:root{text-decoration: underline;text-underline-position:under;}
+.ht{margin:4px; padding:0.7rem;background:#efe;border-radius:4px;a{color:green;text-decoration:none;}}
+.btn{background-image:url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><rect width="48" height="48" fill="none"/><line x1="24" y1="8" x2="24" y2="40" stroke="black" stroke-width="6"/><line x1="8" y1="24" x2="40" y2="24" stroke="black" stroke-width="6"/></svg>');
 background-size:100% 100%;background-repeat:no-repeat;
+width:40px;height:40px;overflow:hidden;position:fixed;
+top:10px;right:3%;z-index:1;display:inline-block;transition:0.8s;
 }
 #btn{display:none}
 #btn:checked~.btn{transform: rotate(-0.87turn);}
-#btn:checked~#menu{height:100%;#data{background:white;}}
+#btn:checked~#menu{height:80%;#data{background:white;}}
 #menu{
-  height:0;
-  transition:1s;
-  width:82%;
+  font-family: monospace;
+  height:62px;
+  transition:0.8s;
+  width:100%;
   margin:0;
   padding:0;
-  position:fixed;left:0;top:0;bottom:0;
+  position:fixed;left:0;top:0px;
   background: linear-gradient(60deg,green,yellow,red);
 }
 #data{
-  transition:1s;
-  position:absolute;
-  left:4px;top:4px;right:4px;bottom:4px;
+  transition:0.8s;
+  position:absolute;border-radius:4px;
+  left:4px;top:3.9em;right:4px;bottom:6px;
   overflow:scroll;
 }
 </style>
 <input id="btn" type="checkbox">
 <label for="btn" class="btn"></label>
-<div id="menu"></div>
+<div id="menu"><h1 class="ht"><a href="#">urln.github.io</a></h1></div>
 `;
-
 document.body.insertAdjacentHTML("beforeend", buttonM);
 
-const content = document.getElementById("menu");
-const divE = document.createElement("div");
+setTimeout(function () {
+  $id("btn").checked = true;
+  setTimeout(function () {
+    $id("btn").checked = false;
+    setTimeout(function () {
+      $id("btn").checked = true;
+      setTimeout(function () {
+        $id("btn").checked = false;
+      }, 3000);
+    }, 3000);
+  }, 3000);
+}, 3000);
+
+const content = $id("menu");
+const divE = $new("div");
 divE.setAttribute("id", "data");
 divE.textContent = "Loading......";
 content.appendChild(divE);
-
 divE.style.fontFamily = "sans-serif";
 divE.style.margin = "0";
 divE.style.padding = "0";
@@ -50,9 +66,9 @@ fetch(url)
     return response.json();
   })
   .then((data) => {
-    const dataDiv = document.getElementById("data");
+    const dataDiv = $id("data");
     dataDiv.innerHTML = "";
-    const ul = document.createElement("ul");
+    const ul = $new("ul");
     ul.style.listStyleType = "none";
     ul.style.margin = "0";
     ul.style.padding = "0";
@@ -64,13 +80,13 @@ fetch(url)
         ""
       );
       const eNs = nSt.replace(/.html|.css|.js|.txt|.md|.png/g, "");
-      const li = document.createElement("li");
+      const li = $new("li");
       li.style.backgroundColor = "#eee";
       li.style.margin = "1px 0";
       li.style.padding = "5px";
-      li.style.border = "5px solid #ddd";
-      li.style.borderRadius = "5px";
-      li.innerHTML = `<a style="color:#123456" href="post/${eNs}.html">${eNs}</a> 
+      li.style.border = "4px solid #ddd";
+      li.style.borderRadius = "";
+      li.innerHTML = `<a style="font-family:monospace;color:#123456" href="post/${eNs}.html">${eNs}</a> 
                             <a style="float:right;color:#456789" href="${item.download_url}">raw</a>`;
       ul.appendChild(li);
     });
@@ -78,6 +94,6 @@ fetch(url)
     dataDiv.appendChild(ul);
   })
   .catch((error) => {
-    const dataDiv = document.getElementById("data");
+    const dataDiv = $id("data");
     dataDiv.innerHTML = "Error fetching data: " + error.message;
   });
